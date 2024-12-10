57215f76efdac725a91f98f4fcf3672d7b0a100c
ais-one <aaronjxz@gmail.com>
2024-12-10 23:03:03 +0800

    wip

--- ftp.js
+++ ftp.js
@@ -3,21 +3,23 @@ const FtpSrv = require('ftp-srv');
 const port=8005;
 const ftpServer = new FtpSrv({
     url: "ftp://0.0.0.0:" + port,
-    anonymous: true
+    anonymous: false
 });
 
 ftpServer.on('login', ({ connection, username, password }, resolve, reject) => { 
     if(username === 'foo' && password === 'bar'){
-        connection.on('RTER', (error, fileName) => {
-          console.log('RTER', error)
-        });
-        connection.on('RNTO', (error, fileName) => {
-          console.log('RNTO', error)
-        });
-        connection.on('STOR', (error, fileName) => {
-          console.log('STOR', error)
-        });
-        return resolve({ root: "/root/node-sftpd" });
+        connection.on('RTER', (error, fileName) => console.log('RTER', error));
+        connection.on('RNTO', (error, fileName) => console.log('RNTO', error));
+        connection.on('STOR', (error, fileName) => console.log('STOR', error));
+        connection.on('RNFR', (error, fileName) => console.log('RNFR', error));
+        connection.on('PORT', (error, fileName) => console.log('PORT', error));
+        connection.on('LIST', (error, fileName) => console.log('LIST', error));
+        connection.on('NLST', (error, fileName) => console.log('NLST', error));
+        connection.on('STAT', (error, fileName) => console.log('STAT', error));
+        connection.on('SIZE', (error, fileName) => console.log('SIZE', error));
+        connection.on('RNFR', (error, fileName) => console.log('RNFR', error));
+        connection.on('MDTM', (error, fileName) => console.log('MDTM', error));
+        return resolve({ root: "/root/node-sftpd/data" });
     }
     return reject(new errors.GeneralError('Invalid username or password', 401));
 });

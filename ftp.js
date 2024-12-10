a7feabbc41b2caf855e2ecd4971b17e74a5419ce
ais-one <aaronjxz@gmail.com>
2024-12-10 23:06:42 +0800

    wip

--- ftp.js
+++ ftp.js
@@ -19,6 +19,8 @@ ftpServer.on('login', ({ connection, username, password }, resolve, reject) => {
         connection.on('SIZE', (error, fileName) => console.log('SIZE', error));
         connection.on('RNFR', (error, fileName) => console.log('RNFR', error));
         connection.on('MDTM', (error, fileName) => console.log('MDTM', error));
+        connection.on('PASS', (error, fileName) => console.log('PASSxxx'));
+        
         return resolve({ root: "/root/node-sftpd/data" });
     }
     return reject(new errors.GeneralError('Invalid username or password', 401));
